86ed329efa63a03f980317dc8d73f64239f0c9c7
rumbeiro3 <75800886+rumbeiro3@users.noreply.github.com>
2021-09-16 17:34:01 +0200

    Update HelloWorldiV.java

--- src/com/example/HelloWorldiV.java
+++ src/com/example/HelloWorldiV.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HelloWorldiV {
 
-  //modificacion del archivo en cithub para pull
+ 
   public static void main (String  args[]) {
 		
 		System.out.println("Hello worldIV");
